3c4ad499206057f6d64f8fb9c5c450d2858324d6
yuxr <779339162@qq.com>
2018-03-27 19:18:12 +0800

    托盘程序调整

--- src/SysTrayGuard/Boss/Tray/BossSystemTray.java
+++ src/SysTrayGuard/Boss/Tray/BossSystemTray.java
@@ -17,13 +17,13 @@ import javax.swing.JFrame;
 import SysTrayGuard.Boss.Panel.Login.DesktopCapture;
 
 /**
- * Description:系统守护托盘程序加载
+ * Description:托盘加载
  * Author:俞晓荣(779339162@qq.com)
  * Date:2018/3/27
  */
 public class BossSystemTray implements ActionListener, MouseListener {
 
-  private Image icon;// 图标
+  private Image icon;// 托盘图标
 
   private TrayIcon trayIcon;
 
@@ -33,44 +33,75 @@ public class BossSystemTray implements ActionListener, MouseListener {
 
   private PopupMenu pop = new PopupMenu(); // 弹出菜单
 
-  private MenuItem capture = new MenuItem("capture");
+  /**
+   * 设置
+   */
+  private MenuItem set = new MenuItem("设置");
 
-  private MenuItem show = new MenuItem("open");
+  /**
+   * 获取消息
+   */
+  private MenuItem getMessage = new MenuItem("获取消息");
 
-  private MenuItem exit = new MenuItem("exit");
+  /**
+   * 重新登录
+   */
+  private MenuItem relogin = new MenuItem("重新登录");
 
+  /**
+   * 退出登录
+   */
+  private MenuItem exit = new MenuItem("退出登录");
+
+  /**
+   * Description:初始化托盘
+   * Author:俞晓荣(779339162@qq.com)
+   * Date:2018/3/27
+   * @param frame 托盘所属主窗体
+   */
   public BossSystemTray(DesktopCapture frame) {
     this.frame = frame;
-    // icon = Toolkit.getDefaultToolkit().getImage("./images/xiaomai.png");
     icon = new ImageIcon(this.getClass().getClassLoader().getResource("image/xiaomai.png")).getImage();
-
+    //如果系统支持托盘
     if (SystemTray.isSupported()) {
+      //获取系统托盘
       systemTray = SystemTray.getSystemTray();
-      trayIcon = new TrayIcon(icon, "单击直接截图-EasyCapture", pop);
-      pop.add(capture);
-      pop.add(show);
+      //创建Boss托盘
+      trayIcon = new TrayIcon(icon, "预算管理一体化系统守护", pop);
+      //右击菜单添加菜单选项
+      pop.add(set);
+      pop.add(getMessage);
+      pop.add(relogin);
       pop.add(exit);
 
       try {
+        //将Boss托盘添加到系统托盘
         systemTray.add(trayIcon);
       } catch (AWTException e1) {
         e1.printStackTrace();
         trayIcon.addMouseListener(this);
       }
     }
+
+    //一些事件的添加
     trayIcon.addMouseListener(this);
-    show.addActionListener(this);
+    getMessage.addActionListener(this);
+    relogin.addActionListener(this);
     exit.addActionListener(this);
-    capture.addActionListener(this);
+    set.addActionListener(this);
   }
 
+  /**
+   * 操作事件监听
+   * @param e 事件
+   */
   @Override
   public void actionPerformed(ActionEvent e) {
-    if (e.getSource() == show) {
+    if (e.getSource() == relogin) {
       frame.iconed = false;
       frame.setVisible(true);
       frame.setExtendedState(JFrame.NORMAL);
-    } else if (e.getSource() == capture) {
+    } else if (e.getSource() == set) {
       frame.capture();
     } else {
       System.exit(0);
